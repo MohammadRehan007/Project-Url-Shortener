49fb1cd72d304e8acf946c47fbf1d94c2427f038
MalikaG015 <malikagulati015@gmail.com>
2021-12-09 12:03:55 +0530

    project-2

---
+++ src/controllers/collegeController.js
@@ -0,0 +1,68 @@
+const collegeModel = require('../models/collegeModel');
+
+//---------------------------Validation Functions---------------------------------------------------------
+
+const isValidRequestBody = function (requestBody) {
+    return Object.keys(requestBody).length > 0
+}
+const isValid = function (value) {
+    if (typeof value === 'undefined' || value === null) return false
+    if (typeof value === 'string' && value.trim().length === 0) return false
+    return true;
+}
+
+
+//----------------------------------------------------------------------------------------------------------
+const createCollege = async function (req, res) {
+    try{
+        if (!isValidRequestBody(req.body)) {
+            return res.status(400).send({ status: false, message: 'Invalid request parameters. Please provide college details' })
+        }
+        let {name,fullName,logoLink}=req.body
+        
+        if(!isValid(name)) {
+            res.status(400).send({status: false, message: `name is required`})
+            return
+        }
+
+        if(!isValid(fullName)) {
+            res.status(400).send({status: false, message: `fullName is required`})
+            return
+        }
+
+        if(!isValid(logoLink)) {
+            res.status(400).send({status: false, message: `logoLink is required`})
+            return
+        }
+
+        if(!(/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/.test(logoLink) &&logoLink.includes(".com")==true)) {
+            res.status(400).send({status: false, message: `logoLink is not a valid URL`})
+            return
+        }
+        
+        let createdCollege= await collegeModel.create(req.body)
+        res.status(201).send({ status:true, msg:createdCollege})
+} 
+catch(err){
+    res.status(500).send({status:false,data:err});
+    console.log(err)
+}
+}
+
+
+
+module.exports = { createCollege }
+
+
+
+
+
+
+/*
+min length in mobile number
+line 21 in college comtroller-these are keys or their values?
+how did you got code for logo link verification
+for keys whose unique:true then there too we have to check for duplicacy like email or mobile
+is line 66-69 required in internsController
+*/
+//undefined means something that is not declared
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,27 @@
 const express = require('express');
-
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
-});
+const collegeController = require('../controllers/collegeController');
+const internsController = require('../controllers/internsController');
+
+
+//1)for saving college details
+router.post('/functionUp/Colleges', collegeController.createCollege);
+
+//2) for saving intern setails
+router.post('/functionUp/interns', internsController.createInterns);
+
+
+//3)return list of all interns applied for that college
+router.get("/functionup/collegeDetails",internsController.giveAllInterns)
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
